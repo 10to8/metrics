3731b91dcd17e67ec681f2d98b164fa358466d94
Jose Maria Ruiz <josemaria.ruiz@10to8.com>
2013-08-21 13:42:08 +0000

    Initial script and requirements.

--- pg_staging_connections.py
+++ pg_staging_connections.py
@@ -7,28 +7,26 @@ from statsd import StatsClient
 psql = local['psql']
 echo = local['echo']
 
-REQUEST="SELECT client_addr, count(*)  FROM pg_stat_activity where client_addr='10.7.1.148'  group by client_addr;"
+REQUEST="SELECT client_addr, count(*)  FROM pg_stat_activity group by client_addr;"
 
 
 statsd = StatsClient()
 
 
-def get_connections():
-    output = (echo[REQUEST] | psql[ '-t', '-a', '-F":"', 'template1'])().split('\n')[1:2][0].split('|')
-
-    output = [line.strip() for line in output]
-    key = 'postgresql.connections.10.7.1.148'
-    value = 0
-    if len(output) == 2:
+def update():
+    output = (echo[REQUEST] | psql[ '-t', '-a', '-F":"', 'template1'])().split('\n')[1:-2]
+    output = [[value.strip() for value in line.split('|')] for line in output]
+    for index, (ip, counter) in enumerate(output):
+    	if not ip:
+	    ip = 'localhost'
 	
-	value = int(output[1])
-
-    return (key, value)
+	key = 'postgresql.connections.{0}'.format(ip)
+	value = int(counter)
+	print key," => ", value
+    	statsd.gauge(key, value)
 
 
 if __name__ == "__main__":
     while 1:
-        key, value = get_connections()
-        print key," => ", value
-        statsd.gauge(key, value)
-        time.sleep(1)
+	update()
+    	time.sleep(1)
