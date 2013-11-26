04dda9731aeb8330847f5b90db556d7dd54938e8
Ubuntu <ubuntu@prod.10to8.com>
2013-11-26 10:52:47 +0000

    Use SQLAlchemy instead of processes.

--- pg_staging_connections.py
+++ pg_staging_connections.py
@@ -1,20 +1,33 @@
 import time
 from plumbum import local
 from statsd import StatsClient
+from sqlalchemy import create_engine
 
-
-
-psql = local['psql']
-echo = local['echo']
 
 REQUEST="SELECT client_addr, count(*)  FROM pg_stat_activity group by client_addr;"
 
-
+engine = create_engine('postgresql+psycopg2://postgres@/template1')
 statsd = StatsClient()
 
 
+def update2():
+    result = engine.execute(REQUEST).fetchall()
+    
+    for index, (ip, counter) in enumerate(result):
+    	if not ip:
+	    ip = 'localhost'
+	
+	key = 'postgresql.connections.{0}'.format(ip)
+	value = int(counter)
+	print key," => ", value
+    	statsd.gauge(key, value)
+
+
+
 def update():
-    output = (echo[REQUEST] | psql[ '-t', '-a', '-F":"', 'template1'])().split('\n')[1:-2]
+    psql = local['psql']
+    echo = local['echo']
+    output = (echo[REQUEST] | psql['-Ustatsd', '-t', '-a', '-F":"', 'template1'])().split('\n')[1:-2]
     output = [[value.strip() for value in line.split('|')] for line in output]
     for index, (ip, counter) in enumerate(output):
     	if not ip:
@@ -28,5 +41,5 @@ def update():
 
 if __name__ == "__main__":
     while 1:
-	update()
-    	time.sleep(1)
+    	update2()
+        time.sleep(1)
