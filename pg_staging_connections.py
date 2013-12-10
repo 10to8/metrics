8acd73a331a22e278183e825b210c0b5edf4b072
Ubuntu <ubuntu@prod.10to8.com>
2013-12-10 10:49:02 +0000

    Add global counter, remove old code.

--- pg_staging_connections.py
+++ pg_staging_connections.py
@@ -1,5 +1,4 @@
 import time
-from plumbum import local
 from statsd import StatsClient
 from sqlalchemy import create_engine
 
@@ -10,36 +9,29 @@ engine = create_engine('postgresql+psycopg2://postgres@/template1')
 statsd = StatsClient()
 
 
-def update2():
+def update():
     result = engine.execute(REQUEST).fetchall()
-    
+    print "-"*50
+    total = 0
+
     for index, (ip, counter) in enumerate(result):
     	if not ip:
 	    ip = 'localhost'
 	
 	key = 'postgresql.connections.{0}'.format(ip)
 	value = int(counter)
-	print key," => ", value
     	statsd.gauge(key, value)
+        total += value
+    
+    key = 'postgresql.total_connections'
+    statsd.gauge(key, total)     
+    
 
 
 
-def update():
-    psql = local['psql']
-    echo = local['echo']
-    output = (echo[REQUEST] | psql['-Ustatsd', '-t', '-a', '-F":"', 'template1'])().split('\n')[1:-2]
-    output = [[value.strip() for value in line.split('|')] for line in output]
-    for index, (ip, counter) in enumerate(output):
-    	if not ip:
-	    ip = 'localhost'
-	
-	key = 'postgresql.connections.{0}'.format(ip)
-	value = int(counter)
-	print key," => ", value
-    	statsd.gauge(key, value)
 
 
 if __name__ == "__main__":
     while 1:
-    	update2()
+    	update()
         time.sleep(1)
